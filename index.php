a365d28722f1879d5acea7b3f5ba47a5c657c1db
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-04-19 12:37:51 +0000

    changes

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!-- $ echo '<?php include_once("index.html"); ?>' > index.php
-$ echo '{}' > composer.json -->
+$ echo '<?php include_once("index.html"); ?>' > index.php
+$ echo '{}' > composer.json
 
-<?php include_once(index.html”); ?>
